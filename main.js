a262e704403c40456460963226bedc8860c65c6a
yomasik <mnsk0504@gmail.com>
2022-07-13 10:45:36 +0900

    Create project

---
+++ main.js
@@ -0,0 +1,11 @@
+const str = `
+010-123-4567
+mnsk0504@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`
+
+console.log(
+    str.match(/(?<=@).{1,}/g)
+)
